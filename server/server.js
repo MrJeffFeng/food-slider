d41e533c3519655932e1cb325d4a4eef8cd6c79f
MrJeffFeng <jefffeng321@gmail.com>
2018-02-03 19:54:48 -0800

    fix

--- server/server.js
+++ server/server.js
@@ -1,10 +1,6 @@
-import bodyParser from 'body-parser'
 import express from 'express'
 import path from 'path'
 const app = express()
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
 
 const router = express.Router()
 
