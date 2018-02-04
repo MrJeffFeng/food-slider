4cc3b5c3aafde2b77298d666de1cd022f03b6efd
MrJeffFeng <jefffeng321@gmail.com>
2018-02-04 03:55:26 -0800

    add

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ router.get('/api/getaccount', (req, res) => {
 
 router.post('/api/check_account', (req, res) => {
   var user = req.body;
-  var sql = 'SELECT acct_id FROM accounts WHERE acct_name = ?' + mysql.escape(user.name);
+  var sql = 'SELECT acct_id FROM accounts WHERE acct_name = ?' + mysql.escape(user.name) + 'and password = ?' + mysql.escape(user.password);
   connection.query(sql, function(err, result){
     console.log(result);
     if (result.length > 0) {
