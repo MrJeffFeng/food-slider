f3a66654ac68ca02b83204fe9d95a5ce66f8d2f5
MrJeffFeng <jefffeng321@gmail.com>
2018-03-27 13:42:53 -0700

    Test

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ router.get('/api/getaccount', (req, res) => {
 
 router.post('/api/check_account', (req, res) => {
   var user = req.body;
-  var sql = 'SELECT acct_id FROM accounts WHERE acct_name = ? and password = ?';
+  var sql = 'SELECT * FROM accounts WHERE acct_name = ? and password = ?';
   connection.query(sql, [user.name, user.password], function(err, result){
     console.log(result);
     if (result.length > 0) {
